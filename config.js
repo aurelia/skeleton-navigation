e1d000377c782b1bfc9c8fce0d247afb0b8702d1
Rob Eisenberg <rob@bluespire.com>
2014-12-18 15:47:36 -0500

    fix(package): update bootstrapper to latest version

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ System.config({
 
 System.config({
   "map": {
-    "aurelia-bootstrapper": "github:aurelia/bootstrapper@0.4.2",
+    "aurelia-bootstrapper": "github:aurelia/bootstrapper@0.4.3",
     "aurelia-http-client": "github:aurelia/http-client@0.3.1",
     "bootstrap": "github:twbs/bootstrap@3.3.1",
     "font-awesome": "npm:font-awesome@4.2.0",
@@ -17,22 +17,22 @@ System.config({
       "aurelia-metadata": "github:aurelia/metadata@0.1.1",
       "aurelia-task-queue": "github:aurelia/task-queue@0.1.1"
     },
-    "github:aurelia/bootstrapper@0.4.2": {
-      "aurelia-framework": "github:aurelia/framework@0.3.1",
+    "github:aurelia/bootstrapper@0.4.3": {
+      "aurelia-framework": "github:aurelia/framework@0.3.2",
       "aurelia-history": "github:aurelia/history@0.1.1",
       "aurelia-history-browser": "github:aurelia/history-browser@0.1.1",
       "aurelia-loader-systemjs": "github:aurelia/loader-systemjs@0.2.2",
       "aurelia-logging-console": "github:aurelia/logging-console@0.1.1",
       "aurelia-router": "github:aurelia/router@0.1.1",
-      "aurelia-templating-binding": "github:aurelia/templating-binding@0.3.1",
-      "aurelia-templating-resources": "github:aurelia/templating-resources@0.3.1",
+      "aurelia-templating-binding": "github:aurelia/templating-binding@0.3.2",
+      "aurelia-templating-resources": "github:aurelia/templating-resources@0.3.2",
       "aurelia-templating-router": "github:aurelia/templating-router@0.4.1"
     },
     "github:aurelia/dependency-injection@0.1.1": {
       "aurelia-metadata": "github:aurelia/metadata@0.1.1",
       "es6-shim": "github:paulmillr/es6-shim@0.21.1"
     },
-    "github:aurelia/framework@0.3.1": {
+    "github:aurelia/framework@0.3.2": {
       "aurelia-binding": "github:aurelia/binding@0.1.2",
       "aurelia-dependency-injection": "github:aurelia/dependency-injection@0.1.1",
       "aurelia-event-aggregator": "github:aurelia/event-aggregator@0.1.1",
@@ -40,7 +40,7 @@ System.config({
       "aurelia-logging": "github:aurelia/logging@0.1.1",
       "aurelia-metadata": "github:aurelia/metadata@0.1.1",
       "aurelia-task-queue": "github:aurelia/task-queue@0.1.1",
-      "aurelia-templating": "github:aurelia/templating@0.3.1"
+      "aurelia-templating": "github:aurelia/templating@0.3.2"
     },
     "github:aurelia/history-browser@0.1.1": {
       "aurelia-history": "github:aurelia/history@0.1.1"
@@ -65,22 +65,22 @@ System.config({
       "aurelia-route-recognizer": "github:aurelia/route-recognizer@0.1.1",
       "es6-shim": "github:paulmillr/es6-shim@0.21.1"
     },
-    "github:aurelia/templating-binding@0.3.1": {
+    "github:aurelia/templating-binding@0.3.2": {
       "aurelia-binding": "github:aurelia/binding@0.1.2",
-      "aurelia-templating": "github:aurelia/templating@0.3.1"
+      "aurelia-templating": "github:aurelia/templating@0.3.2"
     },
-    "github:aurelia/templating-resources@0.3.1": {
+    "github:aurelia/templating-resources@0.3.2": {
       "aurelia-binding": "github:aurelia/binding@0.1.2",
       "aurelia-dependency-injection": "github:aurelia/dependency-injection@0.1.1",
-      "aurelia-templating": "github:aurelia/templating@0.3.1",
+      "aurelia-templating": "github:aurelia/templating@0.3.2",
       "es6-shim": "github:paulmillr/es6-shim@0.21.1"
     },
     "github:aurelia/templating-router@0.4.1": {
       "aurelia-dependency-injection": "github:aurelia/dependency-injection@0.1.1",
       "aurelia-router": "github:aurelia/router@0.1.1",
-      "aurelia-templating": "github:aurelia/templating@0.3.1"
+      "aurelia-templating": "github:aurelia/templating@0.3.2"
     },
-    "github:aurelia/templating@0.3.1": {
+    "github:aurelia/templating@0.3.2": {
       "aurelia-binding": "github:aurelia/binding@0.1.2",
       "aurelia-dependency-injection": "github:aurelia/dependency-injection@0.1.1",
       "aurelia-html-template-element": "github:aurelia/html-template-element@0.1.1",
