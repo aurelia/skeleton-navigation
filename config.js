29ecc3315c1bad2b4b2486161ee4f21f8a8c6e12
Rob Eisenberg <rob@bluespire.com>
2015-01-30 13:08:27 -0500

    chore(all): update dependencies

--- config.js
+++ config.js
@@ -12,7 +12,7 @@ System.config({
     "aurelia-bootstrapper": "github:aurelia/bootstrapper@0.9.3",
     "aurelia-framework": "github:aurelia/framework@0.8.5",
     "aurelia-http-client": "github:aurelia/http-client@0.4.3",
-    "aurelia-router": "github:aurelia/router@0.5.2",
+    "aurelia-router": "github:aurelia/router@0.5.3",
     "bootstrap": "github:twbs/bootstrap@3.3.2",
     "font-awesome": "npm:font-awesome@4.3.0",
     "github:aurelia/binding@0.3.2": {
@@ -26,7 +26,7 @@ System.config({
       "aurelia-history-browser": "github:aurelia/history-browser@0.2.3",
       "aurelia-loader-default": "github:aurelia/loader-default@0.4.1",
       "aurelia-logging-console": "github:aurelia/logging-console@0.2.2",
-      "aurelia-router": "github:aurelia/router@0.5.2",
+      "aurelia-router": "github:aurelia/router@0.5.3",
       "aurelia-templating": "github:aurelia/templating@0.8.7",
       "aurelia-templating-binding": "github:aurelia/templating-binding@0.8.4",
       "aurelia-templating-resources": "github:aurelia/templating-resources@0.8.4",
@@ -50,23 +50,23 @@ System.config({
       "core-js": "npm:core-js@0.4.10"
     },
     "github:aurelia/http-client@0.4.3": {
-      "aurelia-path": "github:aurelia/path@0.4.1",
+      "aurelia-path": "github:aurelia/path@0.4.2",
       "core-js": "npm:core-js@0.4.10"
     },
     "github:aurelia/loader-default@0.4.1": {
       "aurelia-loader": "github:aurelia/loader@0.3.3",
       "aurelia-metadata": "github:aurelia/metadata@0.3.1",
-      "aurelia-path": "github:aurelia/path@0.4.1"
+      "aurelia-path": "github:aurelia/path@0.4.2"
     },
     "github:aurelia/loader@0.3.3": {
       "aurelia-html-template-element": "github:aurelia/html-template-element@0.1.2",
       "core-js": "npm:core-js@0.4.10",
       "webcomponentsjs": "github:webcomponents/webcomponentsjs@0.5.4"
     },
-    "github:aurelia/router@0.5.2": {
+    "github:aurelia/router@0.5.3": {
       "aurelia-dependency-injection": "github:aurelia/dependency-injection@0.4.1",
       "aurelia-history": "github:aurelia/history@0.2.2",
-      "aurelia-path": "github:aurelia/path@0.4.1",
+      "aurelia-path": "github:aurelia/path@0.4.2",
       "aurelia-route-recognizer": "github:aurelia/route-recognizer@0.2.2",
       "core-js": "npm:core-js@0.4.10"
     },
@@ -83,8 +83,8 @@ System.config({
     "github:aurelia/templating-router@0.9.2": {
       "aurelia-dependency-injection": "github:aurelia/dependency-injection@0.4.1",
       "aurelia-metadata": "github:aurelia/metadata@0.3.1",
-      "aurelia-path": "github:aurelia/path@0.4.1",
-      "aurelia-router": "github:aurelia/router@0.5.2",
+      "aurelia-path": "github:aurelia/path@0.4.2",
+      "aurelia-router": "github:aurelia/router@0.5.3",
       "aurelia-templating": "github:aurelia/templating@0.8.7"
     },
     "github:aurelia/templating@0.8.7": {
@@ -94,7 +94,7 @@ System.config({
       "aurelia-loader": "github:aurelia/loader@0.3.3",
       "aurelia-logging": "github:aurelia/logging@0.2.2",
       "aurelia-metadata": "github:aurelia/metadata@0.3.1",
-      "aurelia-path": "github:aurelia/path@0.4.1",
+      "aurelia-path": "github:aurelia/path@0.4.2",
       "aurelia-task-queue": "github:aurelia/task-queue@0.2.2",
       "core-js": "npm:core-js@0.4.10"
     },
