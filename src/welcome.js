675d15a153c399f524070e7cb468caae3230febe
Zewa <zewa666@gmail.com>
2015-04-11 16:39:39 +0200

    feat(anim): Add animation example
    This adds example animations for page transitions, validations and repeated lists

--- src/welcome.js
+++ src/welcome.js
@@ -1,9 +1,15 @@
-import {computedFrom} from 'aurelia-framework';
+import {inject, computedFrom} from 'aurelia-framework';
+import {CssAnimator} from 'aurelia-animator-css';
 
+@inject(CssAnimator)
 export class Welcome{
   heading = 'Welcome to the Aurelia Navigation App!';
   firstName = 'John';
   lastName = 'Doe';
+
+  constructor(animator) {
+    this.animator = animator;
+  }
 
   //Getters can't be observed with Object.observe, so they must be dirty checked.
   //However, if you tell Aurelia the dependencies, it no longer needs to dirty check the property.
@@ -14,7 +20,12 @@ export class Welcome{
   }
 
   welcome(){
-    alert(`Welcome, ${this.fullName}!`);
+    if(this.firstName === '') {
+      this.animator.addClass(this.elFirstName, 'input-error');
+    } else {
+      this.animator.removeClass(this.elFirstName, 'input-error');
+      alert(`Welcome, ${this.fullName}!`);
+    }
   }
 }
 
