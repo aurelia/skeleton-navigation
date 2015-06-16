a3cb19ce63f9a53c58c36244f9cca20ead54fb32
Bryan Smith <BryanRSmith@gmail.com>
2015-06-15 21:28:53 -0700

    feat(routes): name all routes

--- src/app.js
+++ src/app.js
@@ -5,9 +5,9 @@ export class App {
   configureRouter(config, router){
     config.title = 'Aurelia';
     config.map([
-      { route: ['','welcome'],  moduleId: './welcome',      nav: true, title:'Welcome' },
-      { route: 'flickr',        moduleId: './flickr',       nav: true, title:'Flickr' },
-      { route: 'child-router',  moduleId: './child-router', nav: true, title:'Child Router' }
+      { route: ['','welcome'], name: 'welcome',      moduleId: './welcome',      nav: true, title:'Welcome' },
+      { route: 'flickr',       name: 'flickr',       moduleId: './flickr',       nav: true, title:'Flickr' },
+      { route: 'child-router', name: 'child-router', moduleId: './child-router', nav: true, title:'Child Router' }
     ]);
 
     this.router = router;
