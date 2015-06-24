e6accba4a533100f56a7040ff4010390bd9f0bad
Ashley Grant <ashley@ashleygrant.com>
2015-06-24 12:42:50 -0400

    fix(build): include source in source maps. resolve #111

--- build/paths.js
+++ build/paths.js
@@ -9,7 +9,6 @@ module.exports = {
   html: appRoot + '**/*.html',
   style: 'styles/**/*.css',
   output: outputRoot,
-  sourceMapRelativePath: '../' + appRoot,
   doc:'./doc',
   e2eSpecsSrc: 'test/e2e/src/*.js',
   e2eSpecsDist: 'test/e2e/dist/'
