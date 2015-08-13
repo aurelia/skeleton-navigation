49be30b885b52b28f674396f51b92e561e9249eb
Shuhel Ahmed <shuhel@ymail.com>
2015-08-13 09:32:26 -0700

    doc(README): add bundling insturctions

--- aureliafile.js
+++ aureliafile.js
@@ -9,7 +9,7 @@ var config = {
         'aurelia-fetch-client',
         'aurelia-router',
         'aurelia-animator-css',
-        'github:aurelia/templating-binding',
+        'github:aurelia/templating-binding', 
         'github:aurelia/templating-resources',
         'github:aurelia/templating-router',
         'github:aurelia/loader-default',
@@ -24,11 +24,8 @@ var config = {
     "dist/app-bundle": {
       pattern: 'dist/*.html',
       options: {
-        inject: {
-          indexFile: 'index.html',
-          destFile: 'dest_index.html'
-        }
-      }
+        inject:  true
+     }
     }
   }
 };
@@ -36,3 +33,18 @@ var config = {
 cli.command('bundle', config);
 cli.command('unbundle', config);
 
+
+/*
+If you don't like your original `index.html` to be modified and want to have a new index file instead with the template bundle injection, use a bundle configuration like this:
+  template: {
+    "dist/app-bundle": {
+      pattern: 'dist/*.html',
+      options: {
+        inject: {
+          indexFile: 'index.html',
+          destFile: 'dest_index.html'
+        }
+      }
+    }
+  }
+*/
