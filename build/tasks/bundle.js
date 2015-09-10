8f815c638465eabdb565fe7314a86e49d3103c11
Shuhel Ahmed <shuhel@ymail.com>
2015-09-11 02:35:25 +0600

    feat(bundle): configure gulp task with aurelia-bundler.

--- build/tasks/bundle.js
+++ build/tasks/bundle.js
@@ -1,73 +1,5 @@
 var gulp = require('gulp');
-var bundle = require('aurelia-bundler');
-
-var config = {
-  force: true,
-  packagepath: '.',
-  bundles: {
-    "dist/view-bundle": {
-      htmlimport: true,
-      includes: 'dist/*.html',
-      options: {
-        inject: {
-          indexfile : 'index.html',
-          destfile : 'dest_index.html',
-        }
-      }
-    }
-  }
-};
-
-
-/*
-var config = {
-  force: true,
-  packagepath: '.',
-  bundles: {
-    "dist/app-build": {
-      includes: [
-        '*',
-        'bootstrap/css/bootstrap.css!text'
-      ],
-      excludes: [
-        'npm:core-js',
-        'github:jspm/nodelibs-process'
-      ],
-      options: {
-        inject: true,
-        minify: true
-      }
-    },
-    "dist/aurelia": {
-      includes: [
-        'aurelia-bootstrapper',
-        'aurelia-fetch-client',
-        'aurelia-router',
-        'aurelia-animator-css',
-        'github:aurelia/templating-binding',
-        'github:aurelia/templating-resources',
-        'github:aurelia/templating-router',
-        'github:aurelia/loader-default',
-        'github:aurelia/history-browser',
-        'github:aurelia/logging-console'
-      ],
-      options: {
-        inject: true,
-        minify: true
-      }
-    },
-    "dist/view-bundle": {
-      htmlimport: true,
-      includes: 'dist/*.html',
-      options: {
-        inject: {
-          indexfile : 'index.html',
-          destfile : 'dest_index.html',
-        }
-      }
-    }
-  }
-};
+var bundler = require('aurelia-bundler');
 
 var config = {
   force: true,
@@ -80,10 +12,6 @@ var config = {
         '*.css!text',
         'bootstrap/css/bootstrap.css!text'
       ],
-      excludes: [
-        'npm:core-js',
-        'github:jspm/nodelibs-process'
-      ],
       options: {
         inject: true,
         minify: true
@@ -106,25 +34,14 @@ var config = {
         inject: true,
         minify: true
       }
-    },
-    "dist/vendor": {
-      includes: [
-        'text',
-        'github:jspm/nodelibs-process'
-      ],
-      options: {
-        inject: true,
-        minify: true
-      }
     }
   }
 };
-*/
 
-gulp.task('bundle', function(cb) {
-  bundle(config).then(function() {
-    cb();
-  }).catch(function(err) {
-    console.log(err);
-  });
+gulp.task('bundle', function() {
+ return bundler.bundle(config);
+});
+
+gulp.task('unbundle', function() {
+ return bundler.unbundle(config);
 });
