ba41020aca17ea21a5494c6f3385d473c5c0f09f
Rob Eisenberg <rob@bluespire.com>
2015-09-11 00:37:58 -0400

    chore(welcome): clean up comments and explanation

--- src/welcome.js
+++ src/welcome.js
@@ -1,4 +1,4 @@
-import {computedFrom} from 'aurelia-framework';
+//import {computedFrom} from 'aurelia-framework';
 
 export class Welcome{
   heading = 'Welcome to the Aurelia Navigation App!';
@@ -6,9 +6,10 @@ export class Welcome{
   lastName = 'Doe';
   previousValue = this.fullName;
 
-  //Getters can't be observed with Object.observe, so they must be dirty checked.
+  //Getters can't be directly observed, so they must be dirty checked.
   //However, if you tell Aurelia the dependencies, it no longer needs to dirty check the property.
-  //To optimize by declaring the properties that this getter is computed from, uncomment the line below.
+  //To optimize by declaring the properties that this getter is computed from, uncomment the line below
+  //as well as the corrresponding import above.
   //@computedFrom('firstName', 'lastName')
   get fullName(){
     return `${this.firstName} ${this.lastName}`;
