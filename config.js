0cfb76dd874c7084630bc272005cbf0676ecaf7f
Rob Eisenberg <rob@bluespire.com>
2015-10-15 13:01:10 -0400

    chore(config): update library refs

--- config.js
+++ config.js
@@ -1,22 +1,22 @@
 System.config({
-  defaultJSExtensions: true,
-  transpiler: "babel",
-  babelOptions: {
+  "defaultJSExtensions": true,
+  "transpiler": "babel",
+  "babelOptions": {
     "optional": [
-      "es7.decorators",
-      "es7.classProperties",
       "runtime"
     ]
   },
-  paths: {
+  "paths": {
     "*": "dist/*",
     "github:*": "jspm_packages/github/*",
     "npm:*": "jspm_packages/npm/*"
-  },
+  }
+});
 
-  map: {
+System.config({
+  "map": {
     "aurelia-animator-css": "github:aurelia/animator-css@0.17.0",
-    "aurelia-binding": "github:aurelia/binding@0.10.0",
+    "aurelia-binding": "github:aurelia/binding@0.10.1",
     "aurelia-bootstrapper": "github:aurelia/bootstrapper@0.18.0",
     "aurelia-dependency-injection": "github:aurelia/dependency-injection@0.11.0",
     "aurelia-event-aggregator": "github:aurelia/event-aggregator@0.9.0",
@@ -25,7 +25,7 @@ System.config({
     "aurelia-history": "github:aurelia/history@0.8.0",
     "aurelia-history-browser": "github:aurelia/history-browser@0.9.0",
     "aurelia-loader": "github:aurelia/loader@0.10.0",
-    "aurelia-loader-default": "github:aurelia/loader-default@0.11.0",
+    "aurelia-loader-default": "github:aurelia/loader-default@0.11.2",
     "aurelia-logging": "github:aurelia/logging@0.8.0",
     "aurelia-metadata": "github:aurelia/metadata@0.9.0",
     "aurelia-pal": "github:aurelia/pal@0.2.0",
@@ -35,13 +35,13 @@ System.config({
     "aurelia-router": "github:aurelia/router@0.13.0",
     "aurelia-task-queue": "github:aurelia/task-queue@0.8.0",
     "aurelia-templating": "github:aurelia/templating@0.16.0",
-    "aurelia-templating-binding": "github:aurelia/templating-binding@0.16.0",
+    "aurelia-templating-binding": "github:aurelia/templating-binding@0.16.1",
     "aurelia-templating-resources": "github:aurelia/templating-resources@0.16.0",
     "aurelia-templating-router": "github:aurelia/templating-router@0.17.0",
     "babel": "npm:babel-core@5.8.25",
     "babel-runtime": "npm:babel-runtime@5.8.25",
     "bootstrap": "github:twbs/bootstrap@3.3.5",
-    "core-js": "npm:core-js@1.2.1",
+    "core-js": "npm:core-js@0.9.18",
     "fetch": "github:github/fetch@0.9.0",
     "font-awesome": "npm:font-awesome@4.4.0",
     "text": "github:systemjs/plugin-text@0.0.2",
@@ -50,7 +50,7 @@ System.config({
       "aurelia-pal": "github:aurelia/pal@0.2.0",
       "aurelia-templating": "github:aurelia/templating@0.16.0"
     },
-    "github:aurelia/binding@0.10.0": {
+    "github:aurelia/binding@0.10.1": {
       "aurelia-metadata": "github:aurelia/metadata@0.9.0",
       "aurelia-pal": "github:aurelia/pal@0.2.0",
       "aurelia-task-queue": "github:aurelia/task-queue@0.8.0",
@@ -61,13 +61,13 @@ System.config({
       "aurelia-framework": "github:aurelia/framework@0.17.0",
       "aurelia-history": "github:aurelia/history@0.8.0",
       "aurelia-history-browser": "github:aurelia/history-browser@0.9.0",
-      "aurelia-loader-default": "github:aurelia/loader-default@0.11.0",
+      "aurelia-loader-default": "github:aurelia/loader-default@0.11.2",
       "aurelia-logging-console": "github:aurelia/logging-console@0.8.0",
       "aurelia-pal": "github:aurelia/pal@0.2.0",
       "aurelia-pal-browser": "github:aurelia/pal-browser@0.2.0",
       "aurelia-router": "github:aurelia/router@0.13.0",
       "aurelia-templating": "github:aurelia/templating@0.16.0",
-      "aurelia-templating-binding": "github:aurelia/templating-binding@0.16.0",
+      "aurelia-templating-binding": "github:aurelia/templating-binding@0.16.1",
       "aurelia-templating-resources": "github:aurelia/templating-resources@0.16.0",
       "aurelia-templating-router": "github:aurelia/templating-router@0.17.0",
       "core-js": "npm:core-js@0.9.18"
@@ -85,7 +85,7 @@ System.config({
       "core-js": "npm:core-js@0.9.18"
     },
     "github:aurelia/framework@0.17.0": {
-      "aurelia-binding": "github:aurelia/binding@0.10.0",
+      "aurelia-binding": "github:aurelia/binding@0.10.1",
       "aurelia-dependency-injection": "github:aurelia/dependency-injection@0.11.0",
       "aurelia-loader": "github:aurelia/loader@0.10.0",
       "aurelia-logging": "github:aurelia/logging@0.8.0",
@@ -101,7 +101,7 @@ System.config({
       "aurelia-pal": "github:aurelia/pal@0.2.0",
       "core-js": "npm:core-js@0.9.18"
     },
-    "github:aurelia/loader-default@0.11.0": {
+    "github:aurelia/loader-default@0.11.2": {
       "aurelia-loader": "github:aurelia/loader@0.10.0",
       "aurelia-metadata": "github:aurelia/metadata@0.9.0",
       "aurelia-pal": "github:aurelia/pal@0.2.0"
@@ -138,13 +138,13 @@ System.config({
     "github:aurelia/task-queue@0.8.0": {
       "aurelia-pal": "github:aurelia/pal@0.2.0"
     },
-    "github:aurelia/templating-binding@0.16.0": {
-      "aurelia-binding": "github:aurelia/binding@0.10.0",
+    "github:aurelia/templating-binding@0.16.1": {
+      "aurelia-binding": "github:aurelia/binding@0.10.1",
       "aurelia-logging": "github:aurelia/logging@0.8.0",
       "aurelia-templating": "github:aurelia/templating@0.16.0"
     },
     "github:aurelia/templating-resources@0.16.0": {
-      "aurelia-binding": "github:aurelia/binding@0.10.0",
+      "aurelia-binding": "github:aurelia/binding@0.10.1",
       "aurelia-dependency-injection": "github:aurelia/dependency-injection@0.11.0",
       "aurelia-loader": "github:aurelia/loader@0.10.0",
       "aurelia-logging": "github:aurelia/logging@0.8.0",
@@ -163,7 +163,7 @@ System.config({
       "aurelia-templating": "github:aurelia/templating@0.16.0"
     },
     "github:aurelia/templating@0.16.0": {
-      "aurelia-binding": "github:aurelia/binding@0.10.0",
+      "aurelia-binding": "github:aurelia/binding@0.10.1",
       "aurelia-dependency-injection": "github:aurelia/dependency-injection@0.11.0",
       "aurelia-loader": "github:aurelia/loader@0.10.0",
       "aurelia-logging": "github:aurelia/logging@0.8.0",
@@ -196,13 +196,8 @@ System.config({
       "process": "github:jspm/nodelibs-process@0.1.2",
       "systemjs-json": "github:systemjs/plugin-json@0.1.0"
     },
-    "npm:core-js@1.2.1": {
-      "fs": "github:jspm/nodelibs-fs@0.1.2",
-      "process": "github:jspm/nodelibs-process@0.1.2",
-      "systemjs-json": "github:systemjs/plugin-json@0.1.0"
-    },
     "npm:font-awesome@4.4.0": {
-      "css": "github:systemjs/plugin-css@0.1.16"
+      "css": "github:systemjs/plugin-css@0.1.18"
     },
     "npm:inherits@2.0.1": {
       "util": "github:jspm/nodelibs-util@0.1.0"
