49b6d9eed1db64b037a1290cdc1a42422f0a5553
Viktor Sotov <viktor@kangapay.com>
2015-11-18 00:21:47 +0200

    Fixed typo

--- build/paths.js
+++ build/paths.js
@@ -1,6 +1,6 @@
 var appRoot = 'src/';
 var outputRoot = 'dist/';
-var exporSrvtRoot = 'export/'
+var exportSrvRoot = 'export/'
 
 module.exports = {
   root: appRoot,
@@ -9,7 +9,7 @@ module.exports = {
   css: appRoot + '**/*.css',
   style: 'styles/**/*.css',
   output: outputRoot,
-  exportSrv: exporSrvtRoot,
+  exportSrv: exportSrvRoot,
   doc: './doc',
   e2eSpecsSrc: 'test/e2e/src/*.js',
   e2eSpecsDist: 'test/e2e/dist/'
