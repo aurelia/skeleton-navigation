c051b2cb93f2e8d24b4c224af40f56cba6949955
Rob Eisenberg <rob@bluespire.com>
2015-12-04 22:29:25 -0500

    fix(main): add notes on swap order
    Fixes https://github.com/aurelia/templating/issues/226

--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ export function configure(aurelia) {
 
   //Uncomment the line below to enable animation.
   //aurelia.use.plugin('aurelia-animator-css');
+  //if the css animator is enabled, add swap-order="after" to all router-view elements
 
   //Anyone wanting to use HTMLImports to load views, will need to install the following plugin.
   //aurelia.use.plugin('aurelia-html-import-template-loader')
