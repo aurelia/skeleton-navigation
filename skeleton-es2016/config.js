a93f8654ff2ef887e411e4b168f2d299ddacf888
Rob Eisenberg <rob@bluespire.com>
2016-01-30 21:36:37 -0500

    fix(all): lock jspm versions so it will work with our config
    There are bugs in newer jspm releases that break things. We need to
    lock the version, so at least for installation, it will get things
    right. After installation is complete, updating jspm would be ok.

--- skeleton-es2016/config.js
+++ skeleton-es2016/config.js
@@ -17,7 +17,7 @@ System.config({
     "aurelia-animator-css": "npm:aurelia-animator-css@1.0.0-beta.1.1.0",
     "aurelia-bootstrapper": "npm:aurelia-bootstrapper@1.0.0-beta.1.1.1",
     "aurelia-fetch-client": "npm:aurelia-fetch-client@1.0.0-beta.1.1.0",
-    "aurelia-framework": "npm:aurelia-framework@1.0.0-beta.1.1.0",
+    "aurelia-framework": "npm:aurelia-framework@1.0.0-beta.1.1.1",
     "aurelia-history-browser": "npm:aurelia-history-browser@1.0.0-beta.1.1.1",
     "aurelia-loader-default": "npm:aurelia-loader-default@1.0.0-beta.1.1.1",
     "aurelia-logging-console": "npm:aurelia-logging-console@1.0.0-beta.1.1.3",
@@ -55,7 +55,7 @@ System.config({
       "aurelia-pal": "npm:aurelia-pal@1.0.0-beta.1.1.1",
       "aurelia-templating": "npm:aurelia-templating@1.0.0-beta.1.1.0"
     },
-    "npm:aurelia-binding@1.0.0-beta.1.1.0": {
+    "npm:aurelia-binding@1.0.0-beta.1.1.1": {
       "aurelia-metadata": "npm:aurelia-metadata@1.0.0-beta.1.1.3",
       "aurelia-pal": "npm:aurelia-pal@1.0.0-beta.1.1.1",
       "aurelia-task-queue": "npm:aurelia-task-queue@1.0.0-beta.1.1.0",
@@ -63,7 +63,7 @@ System.config({
     },
     "npm:aurelia-bootstrapper@1.0.0-beta.1.1.1": {
       "aurelia-event-aggregator": "npm:aurelia-event-aggregator@1.0.0-beta.1.1.0",
-      "aurelia-framework": "npm:aurelia-framework@1.0.0-beta.1.1.0",
+      "aurelia-framework": "npm:aurelia-framework@1.0.0-beta.1.1.1",
       "aurelia-history": "npm:aurelia-history@1.0.0-beta.1.1.1",
       "aurelia-history-browser": "npm:aurelia-history-browser@1.0.0-beta.1.1.1",
       "aurelia-loader-default": "npm:aurelia-loader-default@1.0.0-beta.1.1.1",
@@ -89,8 +89,8 @@ System.config({
     "npm:aurelia-fetch-client@1.0.0-beta.1.1.0": {
       "core-js": "npm:core-js@2.0.3"
     },
-    "npm:aurelia-framework@1.0.0-beta.1.1.0": {
-      "aurelia-binding": "npm:aurelia-binding@1.0.0-beta.1.1.0",
+    "npm:aurelia-framework@1.0.0-beta.1.1.1": {
+      "aurelia-binding": "npm:aurelia-binding@1.0.0-beta.1.1.1",
       "aurelia-dependency-injection": "npm:aurelia-dependency-injection@1.0.0-beta.1.1.2",
       "aurelia-loader": "npm:aurelia-loader@1.0.0-beta.1.1.0",
       "aurelia-logging": "npm:aurelia-logging@1.0.0-beta.1.1.1",
@@ -144,12 +144,12 @@ System.config({
       "aurelia-pal": "npm:aurelia-pal@1.0.0-beta.1.1.1"
     },
     "npm:aurelia-templating-binding@1.0.0-beta.1.1.0": {
-      "aurelia-binding": "npm:aurelia-binding@1.0.0-beta.1.1.0",
+      "aurelia-binding": "npm:aurelia-binding@1.0.0-beta.1.1.1",
       "aurelia-logging": "npm:aurelia-logging@1.0.0-beta.1.1.1",
       "aurelia-templating": "npm:aurelia-templating@1.0.0-beta.1.1.0"
     },
     "npm:aurelia-templating-resources@1.0.0-beta.1.1.0": {
-      "aurelia-binding": "npm:aurelia-binding@1.0.0-beta.1.1.0",
+      "aurelia-binding": "npm:aurelia-binding@1.0.0-beta.1.1.1",
       "aurelia-dependency-injection": "npm:aurelia-dependency-injection@1.0.0-beta.1.1.2",
       "aurelia-loader": "npm:aurelia-loader@1.0.0-beta.1.1.0",
       "aurelia-logging": "npm:aurelia-logging@1.0.0-beta.1.1.1",
@@ -169,7 +169,7 @@ System.config({
       "aurelia-templating": "npm:aurelia-templating@1.0.0-beta.1.1.0"
     },
     "npm:aurelia-templating@1.0.0-beta.1.1.0": {
-      "aurelia-binding": "npm:aurelia-binding@1.0.0-beta.1.1.0",
+      "aurelia-binding": "npm:aurelia-binding@1.0.0-beta.1.1.1",
       "aurelia-dependency-injection": "npm:aurelia-dependency-injection@1.0.0-beta.1.1.2",
       "aurelia-loader": "npm:aurelia-loader@1.0.0-beta.1.1.0",
       "aurelia-logging": "npm:aurelia-logging@1.0.0-beta.1.1.1",
