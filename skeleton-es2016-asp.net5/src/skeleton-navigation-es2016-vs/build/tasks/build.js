a62fbbb9f59e878aba682c6510b3dd95208b51a0
Rob Eisenberg <rob@bluespire.com>
2016-03-09 15:43:30 -0500

    fix(all): source map configuration

--- skeleton-es2016-asp.net5/src/skeleton-navigation-es2016-vs/build/tasks/build.js
+++ skeleton-es2016-asp.net5/src/skeleton-navigation-es2016-vs/build/tasks/build.js
@@ -19,7 +19,7 @@ gulp.task('build-system', function() {
     .pipe(changed(paths.output, {extension: '.js'}))
     .pipe(sourcemaps.init({loadMaps: true}))
     .pipe(to5(assign({}, compilerOptions, {modules: 'system'})))
-    .pipe(sourcemaps.write({includeContent: true}))
+    .pipe(sourcemaps.write({includeContent: false, sourceRoot: '/src'}))
     .pipe(gulp.dest(paths.output));
 });
 
