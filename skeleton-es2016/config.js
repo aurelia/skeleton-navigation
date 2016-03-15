73e357984766fee2b980f08b28a92454021ec435
Rob Eisenberg <rob@bluespire.com>
2016-03-15 16:54:33 -0400

    chore(package): future dep prep

--- skeleton-es2016/config.js
+++ skeleton-es2016/config.js
@@ -142,6 +142,7 @@ System.config({
       "aurelia-templating": "npm:aurelia-templating@1.0.0-beta.1.1.4"
     },
     "npm:aurelia-templating-resources@1.0.0-beta.1.1.3": {
+      "aurelia-metadata": "npm:aurelia-metadata@1.0.0-beta.1.1.6",
       "aurelia-binding": "npm:aurelia-binding@1.0.0-beta.1.2.2",
       "aurelia-dependency-injection": "npm:aurelia-dependency-injection@1.0.0-beta.1.1.5",
       "aurelia-loader": "npm:aurelia-loader@1.0.0-beta.1.1.1",
