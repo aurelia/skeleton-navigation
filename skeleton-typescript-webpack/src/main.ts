9a9a9238fc619c9f6b4f97cfc2326548f2c2385a
arjendeblok <arjen@deblok.org>
2016-03-18 12:41:01 +0100

    Added CSS back

--- skeleton-typescript-webpack/src/main.ts
+++ skeleton-typescript-webpack/src/main.ts
@@ -1,9 +1,9 @@
 ﻿import {Aurelia} from 'aurelia-framework';
 import {bootstrap} from 'aurelia-bootstrapper-webpack';
 
-//import '../node_modules/bootstrap/dist/css/bootstrap.css';
-//import '../node_modules/font-awesome/css/font-awesome.css';
-//import '../styles/styles.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import '../node_modules/font-awesome/css/font-awesome.css';
+import '../styles/styles.css';
 
 bootstrap((aurelia:any) : void => {
   aurelia.use
