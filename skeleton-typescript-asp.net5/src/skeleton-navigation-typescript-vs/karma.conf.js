1e0cba9b7608974911afd470b44b72058440b989
Rob Eisenberg <rob@bluespire.com>
2016-03-19 11:27:03 -0400

    fix(karma): correct path mapping in config

--- skeleton-typescript-asp.net5/src/skeleton-navigation-typescript-vs/karma.conf.js
+++ skeleton-typescript-asp.net5/src/skeleton-navigation-typescript-vs/karma.conf.js
@@ -6,7 +6,7 @@ module.exports = function(config) {
       configFile: 'wwwroot/config.js',
       config: {
         paths: {
-          "*": null,
+          "*": "*",
           "src/*": "src/*",
           "typescript": "node_modules/typescript/lib/typescript.js",
           "systemjs": "node_modules/systemjs/dist/system.js",
