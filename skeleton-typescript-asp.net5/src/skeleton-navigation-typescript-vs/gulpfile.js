bd94483d95a23365f4ceb113f1ba2309b0a6f84b
nforcce <stefan.wolfsteiner@gmail.com>
2016-05-13 17:10:41 +0200

    Fixed TypeScript transpile in Visual Studio build
    BeforeBuild binding for visual studio was set to 'build-html, build-css, build-jspm' instead of just using 'build'. 'build-system' was missing and so TypeScript files were not transpiled.

--- skeleton-typescript-asp.net5/src/skeleton-navigation-typescript-vs/gulpfile.js
+++ skeleton-typescript-asp.net5/src/skeleton-navigation-typescript-vs/gulpfile.js
@@ -1,4 +1,4 @@
-﻿/// <binding BeforeBuild='build-html, build-css, build-jspm' Clean='clean' />
+﻿/// <binding BeforeBuild='build' Clean='clean' />
 // all gulp tasks are located in the ./build/tasks directory
 // gulp configuration is in files in ./build directory
 require('require-dir')('build/tasks');
