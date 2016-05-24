1f15fe4d7f9fcc9672c03a457dcf736289e5bd30
Daniel Bendel <daniel.bendel@gmail.com>
2016-05-24 11:03:06 +0200

    refactor(typescript-asp.net5): remove browser.d.ts and reference index.d.ts since these are the same and index.d.ts is generated

--- skeleton-typescript-asp.net5/src/skeleton-navigation-typescript-vs/typings/browser.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference path="globals\angular-protractor\index.d.ts" />
-/// <reference path="globals\es6-promise\index.d.ts" />
-/// <reference path="globals\jasmine\index.d.ts" />
-/// <reference path="globals\selenium-webdriver\index.d.ts" />
