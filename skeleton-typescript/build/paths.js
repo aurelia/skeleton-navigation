acb152299e4ece1ba42fe2343e8896ec6486c91b
Daniel Bendel <daniel.bendel@gmail.com>
2016-05-24 10:40:12 +0200

    fix(typescript): apply typings v1 changes

--- skeleton-typescript/build/paths.js
+++ skeleton-typescript/build/paths.js
@@ -14,7 +14,7 @@ module.exports = {
   e2eSpecsSrc: 'test/e2e/src/**/*.ts',
   e2eSpecsDist: 'test/e2e/dist/',
   dtsSrc: [
-    './typings/browser/**/*.d.ts',
+    './typings/globals/**/*.d.ts',
     './custom_typings/**/*.d.ts',
     './jspm_packages/**/*.d.ts'
   ]
