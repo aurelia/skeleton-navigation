f49b81640f97895aefb371b6f15c05f812194309
Mark Stega <mark2002@stega.us>
2016-06-08 18:00:11 -0400

    Change to index.html style of view rather than MVC

--- skeleton-typescript-aspnetcore/src/skeleton-navigation-typescript-vs/wwwroot/config.js
+++ skeleton-typescript-aspnetcore/src/skeleton-navigation-typescript-vs/wwwroot/config.js
@@ -6,6 +6,7 @@ System.config({
     "github:*": "jspm_packages/github/*",
     "npm:*": "jspm_packages/npm/*"
   },
+
   meta: {
     "bootstrap": {
       "deps": [
@@ -13,6 +14,7 @@ System.config({
       ]
     }
   },
+
   map: {
     "aurelia-animator-css": "npm:aurelia-animator-css@1.0.0-beta.1.2.0",
     "aurelia-bootstrapper": "npm:aurelia-bootstrapper@1.0.0-beta.1.2.0",
