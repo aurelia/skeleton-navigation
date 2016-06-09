02ad67b9d139e13cd95418961680ab48a21c215b
Rob Eisenberg <rob@bluespire.com>
2016-06-09 20:39:12 +0200

    chore(all): remove old asp.net skeletons
    These have been replaced by new aspnetcore skeletons.

--- skeleton-es2016-asp.net5/src/skeleton-navigation-es2016-vs/gulpfile.js
+++
@@ -1,4 +0,0 @@
-/// <binding BeforeBuild='clean, build-system, build-html, build-css, build-jspm' Clean='clean' ProjectOpened='watch' />
-// all gulp tasks are located in the ./build/tasks directory
-// gulp configuration is in files in ./build directory
-require('require-dir')('build/tasks');
