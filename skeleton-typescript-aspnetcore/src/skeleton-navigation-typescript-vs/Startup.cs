255269e6e3bf123abc8a771de9337983e7259a36
Greg Gum <greg.gum@gmail.com>
2016-06-20 14:06:43 -0400

    Update Startup.cs
    fix(skeleton_navigation_typescript_aspnetcore): comment out MVC code
    
    The typescript_aspnetcore startup code included references to MVC so it was attempting to use the default route of /home/index instead of the Aurelia startup file index.html. Commented out the MVC code so it can be used again later if needed.  Added code to serve default pages.
    
    This closes issue #517.

--- skeleton-typescript-aspnetcore/src/skeleton-navigation-typescript-vs/Startup.cs
+++ skeleton-typescript-aspnetcore/src/skeleton-navigation-typescript-vs/Startup.cs
@@ -35,7 +35,7 @@ namespace skeleton_navigation_typescript_vs
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            // Add framework services.
+            // Add framework services. 
 
             services.AddApplicationInsightsTelemetry(Configuration);
 
