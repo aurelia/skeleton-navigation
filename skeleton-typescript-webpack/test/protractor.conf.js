a80d2f223bb47cb9a738794968d1c4a2812185b4
Bazyli Brzóska <bazyli.brzoska@gmail.com>
2016-07-13 18:08:11 +0200

    fix(typescript-webpack): do not type-check tests when running

--- skeleton-typescript-webpack/test/protractor.conf.js
+++ skeleton-typescript-webpack/test/protractor.conf.js
@@ -28,7 +28,7 @@ exports.config = {
   },
 
   onPrepare: function() {
-    require('ts-babel-node-extendable').register({ compilerOptions: { allowJs: false }});
+    require('ts-babel-node-extendable').register({ compilerOptions: { allowJs: false }, disableWarnings: true, fast: true });
   },
 
   plugins: [{
