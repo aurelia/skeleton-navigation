d0f6c3165a1d9c25fc85f0600243af2216226e66
Ashley Grant <ashley@ashleygrant.com>
2016-09-25 12:26:16 -0400

    chore(electron): disable electron node integration to fix jquery

--- skeleton-typescript/index.js
+++ skeleton-typescript/index.js
@@ -18,14 +18,21 @@ app.on('window-all-closed', () => {
 app.on('ready', () => {
   mainWindow = new BrowserWindow({
     width: 800,
-    height: 600
+    height: 600,
+    // Note: The following line turns off Node integration to allow 
+    // jQuery to work properly. If you need Node integration, please
+    // see the Electron FAQ for how to enable this:
+    // http://electron.atom.io/docs/faq/
+    webPreferences: {
+      nodeIntegration: false
+    }
   });
 
   mainWindow.loadURL(`file://${__dirname}/index.html`);
   mainWindow.webContents.on('did-finish-load', () => {
     mainWindow.setTitle(app.getName());
   });
-  
+
   mainWindow.on('closed', () => {
     mainWindow = null;
   });
