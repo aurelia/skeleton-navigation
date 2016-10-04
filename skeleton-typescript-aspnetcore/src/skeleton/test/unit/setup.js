aa5f97c2bcc6d6ce12b23826ea37ae9b3af1eff3
Patrick Walters <patrickwaltersc21@yahoo.com>
2016-10-04 00:40:14 -0500

    chore(artifacts): remove visual studio artifacts (#678)

--- skeleton-typescript-aspnetcore/src/skeleton/test/unit/setup.js
+++
@@ -1,5 +0,0 @@
-define(["require", "exports", 'aurelia-pal-browser', 'aurelia-polyfills'], function (require, exports, aurelia_pal_browser_1) {
-    "use strict";
-    aurelia_pal_browser_1.initialize();
-});
-//# sourceMappingURL=setup.js.map
