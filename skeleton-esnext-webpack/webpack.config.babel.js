9c2da708435c051470dea12109c6dc2aa9819185
Bazyli Brzóska <bazyli.brzoska@gmail.com>
2016-12-10 14:07:13 +0100

    fix(esnext-webpack): use latest babel-preset-env and fix uglify

--- skeleton-esnext-webpack/webpack.config.babel.js
+++ skeleton-esnext-webpack/webpack.config.babel.js
@@ -97,7 +97,7 @@ let config = generateConfig(
 
   aurelia({root: rootDir, src: srcDir, title: title, baseUrl: baseUrl}),
 
-  babel(),
+  babel({ options: { /* uses settings from .babelrc */ } }),
   html(),
   css({ filename: 'styles.css', allChunks: true, sourceMap: false }),
   fontAndImages(),
