9c40aab3f8b63c7daf4c4ace19cf01dd8042fc63
Dale <daleholborow@users.noreply.github.com>
2017-05-10 10:32:30 +0300

    fix(typescript): fix for Unknown compiler option 'compileOnSave' (#827)
    "Unknown compiler option 'compileOnSave' #824 was being caused by incorrectly declared compileOnSave in wrong area
    
    * fix-824: bump to get appveyor to rebuild

--- skeleton-typescript/config.js
+++ skeleton-typescript/config.js
@@ -193,6 +193,9 @@ System.config({
     "npm:inherits@2.0.1": {
       "util": "github:jspm/nodelibs-util@0.1.0"
     },
+    "npm:isarray@1.0.0": {
+      "systemjs-json": "github:systemjs/plugin-json@0.1.2"
+    },
     "npm:process@0.11.9": {
       "assert": "github:jspm/nodelibs-assert@0.1.0",
       "fs": "github:jspm/nodelibs-fs@0.1.2",
