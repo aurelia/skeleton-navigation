20fc09a0270c15545cd668ba6872ca4ad2951ecc
Landan <lmj0011@users.noreply.github.com>
2017-07-20 01:06:36 -0500

    Update protractor.conf.js (#773)

--- skeleton-esnext/protractor.conf.js
+++ skeleton-esnext/protractor.conf.js
@@ -9,7 +9,8 @@ exports.config = {
 
   // optional: add seleniumServerJar with proper version number
   // seleniumServerJar: './node_modules/gulp-protractor/node_modules/protractor/selenium/selenium-server-standalone-2.53.1.jar',
-  specs: ['test/e2e/dist/**/*.js'],
+  
+  // specs: ['test/e2e/dist/**/*.js'], NOTE - see the e2e gulp task for specifying specs location
 
   plugins: [{
     package: 'aurelia-protractor-plugin'
